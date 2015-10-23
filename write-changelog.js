647e6fa2f89a472471d7862468b47e0bada5db59
Jamie Mason <siunmaru@gmail.com>
2015-10-23 16:30:36 +0100

    chore(workflow): Update formatters, linters

--- write-changelog.js
+++ write-changelog.js
@@ -1,13 +1,19 @@
+#!/usr/bin/env node
+
+'use strict';
+
 var changelog = require('conventional-changelog');
 var fs = require('fs');
 var pkg = require('./package.json');
 
 changelog({
-  version: pkg.version,
-  repository: 'https://github.com/JamieMason/shrinkpack'
-}, function(err, log) {
-  if (err) {
-    throw new Error(err);
-  }
-  fs.writeFileSync('CHANGELOG.md', log);
-});
+    version: pkg.version,
+    repository: 'https://github.com/JamieMason/shrinkpack'
+}, onChangeLog);
+
+function onChangeLog(err, log) {
+    if (err) {
+        throw new Error(err);
+    }
+    fs.writeFileSync('CHANGELOG.md', log);
+}
