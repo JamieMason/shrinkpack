13b8604b90552ca022a215be23e1343f8d4787c4
Jamie Mason <siunmaru@gmail.com>
2016-04-03 16:09:40 +0100

    feat(resolve): add support for git dependencies
    fixes #12, #18, #19

---
+++ src/exec.js
@@ -0,0 +1,18 @@
+var exec = require('child_process').exec;
+var when = require('when');
+
+module.exports = executeShellScript;
+
+function executeShellScript(command) {
+    return when.promise(function(resolve, reject, notify) {
+        exec(command, function onExec(err, stdout, stderr) {
+            if (err) {
+                reject(err);
+            } else if (stderr) {
+                reject(err);
+            } else {
+                resolve(stdout.trim());
+            }
+        });
+    });
+}
