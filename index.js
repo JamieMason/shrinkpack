59bd0209fdb1e4d26d3c073bccb2403f2428ca3e
Rhiad Jaffar <rhiad@basetagger.com>
2016-04-03 13:57:36 -0600

    fix(resolve): normalize scoped module dependency name
    refs #12

--- index.js
+++ index.js
@@ -143,8 +143,8 @@ function updateShrinkwrap(config) {
     }
 
     function rewritePath(key, object) {
-        if (object.resolved.search(/\.tgz|\.tar\.gz/) !== -1) {
-            object.resolved = './node_shrinkwrap/' + key + '-' + object.version + '.tgz';
+        if (object.resolved.search(/\.tgz|\.tar\.gz|\.git/) !== -1) {
+            object.resolved = './node_shrinkwrap/' + key.replace(/\//g, '-') + '-' + object.version + '.tgz';
         }
     }
 }
