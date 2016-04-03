031a5c79f8b225245256880443e801bafe7593c0
Dylan Piercey <pierceydylan@gmail.com>
2016-04-03 16:11:36 -0600

    Fix git dependency resolution

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ function updateShrinkwrap(config) {
     }
 
     function rewritePath(key, object) {
-        if (object.resolved.search(/\.tgz|\.tar\.gz/) !== -1) {
+        if (object.resolved.search(/\.tgz|\.tar\.gz|\.git/) !== -1) {
             object.resolved = './node_shrinkwrap/' + key.replace(/\//g, '-') + '-' + object.version + '.tgz';
         }
     }
