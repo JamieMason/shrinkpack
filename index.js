51b05f03f00409aa254a6888fad496ddf1012706
Rhiad Jaffar <rhiad@basetagger.com>
2016-04-04 08:37:54 +0100

    style(workflow): switch to semistandard eslint config

--- index.js
+++ index.js
@@ -20,149 +20,148 @@ var shrinkwrap = require('./src/shrinkwrap');
 // run
 
 getNpmCachePath()
-    .then(getConfig)
-    .then(createDirectory)
-    .then(getDeps)
-    .then(prune)
-    .then(getMissingDeps)
-    .then(addMissingDeps)
-    .then(updateShrinkwrap)
-    .then(displaySummary, displayFailure);
+  .then(getConfig)
+  .then(createDirectory)
+  .then(getDeps)
+  .then(prune)
+  .then(getMissingDeps)
+  .then(addMissingDeps)
+  .then(updateShrinkwrap)
+  .then(displaySummary, displayFailure);
 
 // implementation
 
-function getNpmCachePath() {
-    return exec('npm config get cache');
+function getNpmCachePath () {
+  return exec('npm config get cache');
 }
 
-function getConfig(npmCachePath) {
-    var projectPath = process.env.PWD || process.cwd();
-    var graphPath = path.join(projectPath, 'npm-shrinkwrap.json');
-    var graph = require(graphPath);
-    var shrinkpackPath = path.join(projectPath, 'node_shrinkwrap');
+function getConfig (npmCachePath) {
+  var projectPath = process.env.PWD || process.cwd();
+  var graphPath = path.join(projectPath, 'npm-shrinkwrap.json');
+  var graph = require(graphPath);
+  var shrinkpackPath = path.join(projectPath, 'node_shrinkwrap');
 
-    return {
-        deps: [],
-        graph: graph,
-        missingDeps: [],
-        prunedPaths: [],
-        path: {
-            project: projectPath,
-            graph: graphPath,
-            shrinkpack: shrinkpackPath,
-            npmCache: npmCachePath
-        }
-    };
-}
-
-function createDirectory(config) {
-    if (!fs.existsSync(config.path.shrinkpack)) {
-        fs.mkdirSync(config.path.shrinkpack);
+  return {
+    deps: [],
+    graph: graph,
+    missingDeps: [],
+    prunedPaths: [],
+    path: {
+      project: projectPath,
+      graph: graphPath,
+      shrinkpack: shrinkpackPath,
+      npmCache: npmCachePath
     }
+  };
+}
+
+function createDirectory (config) {
+  if (!fs.existsSync(config.path.shrinkpack)) {
+    fs.mkdirSync(config.path.shrinkpack);
+  }
+  return config;
+}
+
+function getDeps (config) {
+  config.deps = shrinkwrap.toArray(config);
+  return config;
+}
+
+function prune (config) {
+  return shrinkpack.prune(config)
+    .then(function (prunedPaths) {
+      config.prunedPaths = prunedPaths;
+      return config;
+    }, function (err) {
+      console.error(err);
+      return err;
+    }, function (msg) {
+      console.info(chalk.red(msg));
+      return msg;
+    });
+}
+
+function getMissingDeps (config) {
+  config.missingDeps = shrinkpack.getMissingPackages(config);
+  return config;
+}
+
+function addMissingDeps (config) {
+  return when.all(
+    config.missingDeps.map(addMissingDep)
+  ).then(function () {
     return config;
+  });
+
+  function addMissingDep (dep) {
+    return when(dep)
+      .then(addToNpmCache)
+      .then(addToShrinkpack);
+  }
+
+  function addToNpmCache (dep) {
+    return npmCache.ensure(dep)
+      .then(function () {
+        return dep;
+      }, function (err) {
+        console.error(err);
+        return err;
+      }, function (msg) {
+        return msg;
+      });
+  }
+
+  function addToShrinkpack (dep) {
+    return shrinkpack.addPackage(dep)
+      .then(function () {
+        return dep;
+      }, function (err) {
+        console.error(err);
+        return err;
+      }, function (msg) {
+        console.info(chalk.green(msg));
+        return msg;
+      });
+  }
 }
 
-function getDeps(config) {
-    config.deps = shrinkwrap.toArray(config);
-    return config;
-}
+function updateShrinkwrap (config) {
+  rewritePaths();
 
-function prune(config) {
-    return shrinkpack.prune(config)
-        .then(function(prunedPaths) {
-            config.prunedPaths = prunedPaths;
-            return config;
-        }, function(err) {
-            console.error(err);
-            return err;
-        }, function(msg) {
-            console.info(chalk.red(msg));
-            return msg;
-        });
-}
-
-function getMissingDeps(config) {
-    config.missingDeps = shrinkpack.getMissingPackages(config);
-    return config;
-}
-
-function addMissingDeps(config) {
-    return when.all(
-        config.missingDeps.map(addMissingDep)
-    ).then(function() {
-        return config;
+  return file.write(config.path.graph, JSON.stringify(config.graph, null, 2))
+    .then(function () {
+      return config;
+    }, function (err) {
+      return err;
+    }, function (msg) {
+      return msg;
     });
 
-    function addMissingDep(dep) {
-        return when(dep)
-            .then(addToNpmCache)
-            .then(addToShrinkpack);
-    }
+  function rewritePaths () {
+    shrinkwrap.recurse(config.graph, rewritePath);
+  }
 
-    function addToNpmCache(dep) {
-        return npmCache.ensure(dep)
-            .then(function() {
-                return dep;
-            }, function(err) {
-                console.error(err);
-                return err;
-            }, function(msg) {
-                return msg;
-            });
+  function rewritePath (key, object) {
+    if (object.resolved.search(/\.tgz|\.tar\.gz|\.git/) !== -1) {
+      object.resolved = './node_shrinkwrap/' + key.replace(/\//g, '-') + '-' + object.version + '.tgz';
     }
-
-    function addToShrinkpack(dep) {
-        return shrinkpack.addPackage(dep)
-            .then(function() {
-                return dep;
-            }, function(err) {
-                console.error(err);
-                return err;
-            }, function(msg) {
-                console.info(chalk.green(msg));
-                return msg;
-            });
-    }
+  }
 }
 
-function updateShrinkwrap(config) {
-
-    rewritePaths();
-
-    return file.write(config.path.graph, JSON.stringify(config.graph, null, 2))
-        .then(function() {
-            return config;
-        }, function(err) {
-            return err;
-        }, function(msg) {
-            return msg;
-        });
-
-    function rewritePaths() {
-        shrinkwrap.recurse(config.graph, rewritePath);
-    }
-
-    function rewritePath(key, object) {
-        if (object.resolved.search(/\.tgz|\.tar\.gz|\.git/) !== -1) {
-            object.resolved = './node_shrinkwrap/' + key.replace(/\//g, '-') + '-' + object.version + '.tgz';
-        }
-    }
+function displaySummary (config) {
+  console.info(
+    'shrinkpack %s %s',
+    chalk.green('+' + config.missingDeps.length),
+    chalk.red('-' + config.prunedPaths.length)
+  );
+  process.exit(0);
 }
 
-function displaySummary(config) {
-    console.info(
-        'shrinkpack %s %s',
-        chalk.green('+' + config.missingDeps.length),
-        chalk.red('-' + config.prunedPaths.length)
-    );
-    process.exit(0);
-}
-
-function displayFailure(err) {
-    console.error(
-        chalk.red('Please raise an issue at %s\n\n%s'),
-        chalk.underline('https://github.com/JamieMason/shrinkpack/issues'),
-        String(err.stack).replace(/^/gm, '    ')
-    );
-    process.exit(1);
+function displayFailure (err) {
+  console.error(
+    chalk.red('Please raise an issue at %s\n\n%s'),
+    chalk.underline('https://github.com/JamieMason/shrinkpack/issues'),
+    String(err.stack).replace(/^/gm, '    ')
+  );
+  process.exit(1);
 }
--- src/file.js
+++ src/file.js
@@ -5,77 +5,75 @@
 var fs = require('fs');
 var when = require('when');
 
-// Public
+// public
 
 module.exports = {
-    copy: copyFile,
-    remove: removeFile,
-    write: writeFile
+  copy: copyFile,
+  remove: removeFile,
+  write: writeFile
 };
 
-// Implementation
+// implementation
 
-function copyFile(source, target) {
-    return when.promise(promiseFileCopy);
+function copyFile (source, target) {
+  return when.promise(promiseFileCopy);
 
-    function promiseFileCopy(resolve, reject, notify) {
-        var readStream = fs.createReadStream(source);
-        var writeStream = fs.createWriteStream(target);
+  function promiseFileCopy (resolve, reject, notify) {
+    var readStream = fs.createReadStream(source);
+    var writeStream = fs.createWriteStream(target);
 
-        notify(source);
+    notify(source);
 
-        readStream.on('error', onReadError);
-        writeStream.on('error', onWriteError);
-        writeStream.on('finish', onWriteEnd);
+    readStream.on('error', onReadError);
+    writeStream.on('error', onWriteError);
+    writeStream.on('finish', onWriteEnd);
 
-        readStream.pipe(writeStream);
+    readStream.pipe(writeStream);
 
-        function onReadError(err) {
-            reject('error reading ' + source + ': ' + err.toString());
-        }
-
-        function onWriteError(err) {
-            reject('error writing ' + target + ': ' + err.toString());
-        }
-
-        function onWriteEnd() {
-            resolve(source);
-        }
+    function onReadError (err) {
+      reject('error reading ' + source + ': ' + err.toString());
     }
+
+    function onWriteError (err) {
+      reject('error writing ' + target + ': ' + err.toString());
+    }
+
+    function onWriteEnd () {
+      resolve(source);
+    }
+  }
 }
 
-function removeFile(source) {
-    return when.promise(promiseFileRemove);
+function removeFile (source) {
+  return when.promise(promiseFileRemove);
 
-    function promiseFileRemove(resolve, reject, notify) {
+  function promiseFileRemove (resolve, reject, notify) {
+    notify(source);
+    fs.unlink(source, onFileRemove);
 
-        notify(source);
-        fs.unlink(source, onFileRemove);
-
-        function onFileRemove(err) {
-            if (err) {
-                reject(err.toString());
-            } else {
-                resolve(source);
-            }
-        }
+    function onFileRemove (err) {
+      if (err) {
+        reject(err.toString());
+      } else {
+        resolve(source);
+      }
     }
+  }
 }
 
-function writeFile(target, contents) {
-    return when.promise(promiseFileWrite);
+function writeFile (target, contents) {
+  return when.promise(promiseFileWrite);
 
-    function promiseFileWrite(resolve, reject, notify) {
+  function promiseFileWrite (resolve, reject, notify) {
+    notify(target);
+    fs.writeFile(target, contents, 'utf8', onFileWrite);
 
-        notify(target);
-        fs.writeFile(target, contents, 'utf8', onFileWrite);
-
-        function onFileWrite(err) {
-            if (err) {
-                reject(err.toString());
-            } else {
-                resolve(target);
-            }
-        }
+    function onFileWrite (err) {
+      if (err) {
+        reject(err.toString());
+      } else {
+        resolve(target);
+      }
     }
+  }
 }
