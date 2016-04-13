1aa996420cbc5430651142bbb6fea46258927f3e
Jamie Mason <siunmaru@gmail.com>
2016-04-13 19:32:59 +0100

    feat(shrinkwrap): query registry if unable to patch missing resolved property locally

--- task/resolveTarball.js
+++ task/resolveTarball.js
@@ -2,15 +2,18 @@
 var chalk = require('chalk');
 var glob = require('glob');
 
+// modules
+var shell = require('./lib/shell');
+
 // public
 module.exports = resolveTarball;
 
 // implementation
 function resolveTarball (deps) {
-  return Promise.all(deps.map(resolvePackage));
+  return Promise.all(deps.map(resolveLocally));
 }
 
-function resolvePackage (dep) {
+function resolveLocally (dep) {
   return new Promise(function (resolve, reject) {
     var pkgPath = getPkgPath(dep);
     var pkgJson = getPkgJson(pkgPath);
@@ -19,8 +22,9 @@ function resolvePackage (dep) {
       console.info(chalk.green('✓ set missing "resolved" property for %s to %s'), dep.id, dep.shrinkwrap.resolved);
       resolve();
     } else {
-      console.error(chalk.red('failed to resolve tarball for %s'), dep.id);
-      reject(new Error('failed to resolve tarball for ' + dep.id));
+      console.info(chalk.gray('? %s has no "dist.tarball" in package.json, trying npm registry...'), dep.id);
+      resolveRemotely(dep)
+        .then(resolve, reject);
     }
   });
 }
@@ -34,7 +38,23 @@ function getPkgJson (pkgPath) {
 }
 
 function getPkgPath (dep) {
-  return glob.sync('node_modules/**/' + dep.name + '/**/package.json', {
+  return glob.sync('node_modules/**/' + dep.name + '/package.json', {
     realpath: true
   })[0] || '';
+}
+
+function resolveRemotely (dep) {
+  return shell('npm view ' + dep.id + ' --json')
+    .then(success, fail)
+    .catch(fail);
+
+  function success (stdout) {
+    dep.shrinkwrap.resolved = JSON.parse(stdout).dist.tarball;
+    console.info(chalk.green('✓ set missing "resolved" property for %s to %s'), dep.id, dep.shrinkwrap.resolved);
+  }
+
+  function fail (err) {
+    console.error(chalk.red('! failed to resolve tarball for %s'), dep.id);
+    throw err;
+  }
 }
