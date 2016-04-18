f051d913178515354ec272ab1fde32ed607c0289
Rhiad Jaffar <rhiad@basetagger.com>
2016-04-18 22:52:18 +0100

    fix(shrinkpack): removeFromBundle is provided a tarball path
    fixes #30

--- task/removeFromBundle.js
+++ task/removeFromBundle.js
@@ -8,20 +8,20 @@ var removeFile = require('./lib/removeFile');
 module.exports = removeFromBundle;
 
 // implementation
-function removeFromBundle (deps) {
-  return Promise.all(deps.map(removeDep));
+function removeFromBundle (tarPaths) {
+  return Promise.all(tarPaths.map(removeDep));
 }
 
-function removeDep (dep) {
-  return removeFile(dep.tarball.shrinkpack)
+function removeDep (tarPath) {
+  return removeFile(tarPath)
     .then(success, fail);
 
   function success () {
-    console.info(chalk.red('- %s'), dep.id);
+    console.info(chalk.red('- %s'), tarPath.replace(/.+\//, ''));
   }
 
   function fail (err) {
-    console.error(chalk.red('! failed to remove %s'), dep.id);
+    console.error(chalk.red('! failed to remove %s'), tarPath);
     throw err;
   }
 }
