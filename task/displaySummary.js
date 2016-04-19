d8b99d09e5555d7ab1c808d958faf7ce293ec964
Jamie Mason <siunmaru@gmail.com>
2016-04-19 17:45:04 +0100

    feat(logging): display time taken in summary

--- task/displaySummary.js
+++ task/displaySummary.js
@@ -1,17 +1,21 @@
 // 3rd party modules
 var chalk = require('chalk');
 
+// modules
+var getTimeBetween = require('./lib/getTimeBetween');
+
 // public
 module.exports = displaySummary;
 
 // implementation
 function displaySummary (config) {
-  console.info(
-    'shrinkpack %s %s %s %s',
+  console.info([
+    'shrinkpack',
     chalk.green('+' + config.deps.missingFromBundle.length),
     chalk.red('-' + config.deps.removeFromBundle.length),
     chalk.yellow('↓' + config.deps.missingFromCache.length),
-    chalk.green('✓' + config.deps.unresolved.length)
-  );
+    chalk.green('✓' + config.deps.unresolved.length),
+    chalk.grey(getTimeBetween(config.startTime, new Date()))
+  ].join(' '));
   process.exit(0);
 }
