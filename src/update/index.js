2ebcf925722c19566dc10d6a68c868cd35e06444
Jamie Mason <siunmaru@gmail.com>
2016-08-04 13:57:53 +0100

    fix(shrinkwrap): rewrite npm-shrinkwrap.json on update

--- src/update/index.js
+++ src/update/index.js
@@ -4,22 +4,19 @@ var when = require('when');
 // modules
 var addUsedDependencies = require('./add-used-dependencies');
 var deleteUnusedDependencies = require('./delete-unused-dependencies');
+var rewriteGraph = require('./rewrite-graph');
 
 // public
 module.exports = update;
 
 // implementation
 function update(config) {
-  return when(config)
-    .then(synchronise)
-    .then(function () {
-      return config;
-    });
-
-  function synchronise(config) {
-    return when.join(
-      addUsedDependencies(config),
-      deleteUnusedDependencies(config)
-    );
-  }
+  return when.join(
+    addUsedDependencies(config),
+    deleteUnusedDependencies(config)
+  ).then(function () {
+    return rewriteGraph(config);
+  }).then(function () {
+    return config;
+  });
 }
