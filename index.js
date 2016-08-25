d7ccf2a4d7f83e8a71ebb9872ebcdd2924bad3e4
Jamie Mason <jamie@foldleft.io>
2016-08-25 15:27:34 +0100

    feat(api): expose cli runner through node.js api
    closes #67

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // modules
 var analyse = require('./src/analyse');
+var cli = require('./src/cli');
 var update = require('./src/update');
 
 // public
 module.exports = {
   analyse: analyse,
+  cli: cli.run,
   update: update
 };
