7028c6e4b6f97ebd32e739038fe86f77aceef092
Jamie Mason <jamie@foldleft.io>
2017-11-17 16:47:43 +0000

    feat(npm5): inherit stderr from child processes
    suggestion from @timdp https://github.com/JamieMason/shrinkpack/issues/83#issuecomment-304618694

--- src/lib/npm-pack.ts
+++ src/lib/npm-pack.ts
@@ -3,7 +3,10 @@ import { bug } from './log';
 
 export const npmPack = async (cachePath: string, locator: string) => {
   try {
-    await spawn('npm', ['pack', locator], { cwd: cachePath });
+    await spawn('npm', ['pack', locator], {
+      cwd: cachePath,
+      stdio: ['pipe', 'pipe', 'inherit']
+    });
   } catch (err) {
     bug(`failed to "npm pack ${locator}"`, err);
   }
