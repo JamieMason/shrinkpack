ff6222520149d314e2035612969ac48e62466063
Jamie Mason <jamie@foldleft.io>
2017-12-30 15:47:49 +0000

    fix(npm5): promise was being passed to ssri.fromData

--- src/lib/get-integrity.ts
+++ src/lib/get-integrity.ts
@@ -4,8 +4,8 @@ import { bug } from './log';
 
 export const getIntegrity = async (path: string): Promise<IIntegrity> => {
   try {
-    const data = await readFile(path).toString();
-    return fromData(data);
+    const data = await readFile(path);
+    return fromData(data.toString());
   } catch (err) {
     bug(`failed to get hash integrity of ${path} from zkat/ssri`, err);
     return parse('');
